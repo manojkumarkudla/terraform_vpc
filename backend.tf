e838f59e110044e099ee3e3c8e6dcc4357a463d1
Manoj Kumar <manoj.kumar@cloudreach.com>
2022-09-30 12:37:42 +0530

    backend changes

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "talent-academy-manojkudla-lab-tfstate"
+    bucket         = "talent-academy-manojk-kudla-lab-tfstate"
     key            = "talent-academy/terraform_vpc/terraform.tfstates"
     region         = "eu-west-1"
     dynamodb_table = "terraform-lock"
